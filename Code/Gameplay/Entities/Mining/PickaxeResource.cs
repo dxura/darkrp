78e1e55f9c2121ae91588a720465d33f9ffd66a6
Yağız Akgün <berkay123d@outlook.com>
2024-09-14 19:06:28 +0300

    Mining System
    First version of mining system

---
+++ Code/Gameplay/Entities/Mining/PickaxeResource.cs
@@ -0,0 +1,6 @@
+
+[GameResource("Darkrp/Mining/Pickaxe Item", "pcx", "A basic pickaxe definition", IconBgColor = "#58c0e0", Icon = "ui/equipment/pickaxe.png")]
+public class PickaxeResource : EquipmentResource
+{
+	[Category( "Pickaxe" )] public float PickaxeDamage { get; set; }
+}
