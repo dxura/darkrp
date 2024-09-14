e49d8f879c53c11057eac3939d565ad54109319d
JJ <joshua.jenkins@live.co.uk>
2024-09-14 12:01:58 +0100

    Fixed some Typing issues around the Nourishment resource

--- Code/Resources/Nourishments/NourishmentResource.cs
+++ Code/Resources/Nourishments/NourishmentResource.cs
@@ -3,11 +3,11 @@ public enum NourishmentTypes { Food, Drink };
 [GameResource("Darkrp/Nourishment", "nrsh", "A basic nourishment definition", IconBgColor = "#5877E0", Icon = "restaurant")]
 public class NourishmentResource : GameResource
 {
-  [Category("Display")] public string Name { get; set; }
+  [Category("Display")] public required string Name { get; set; }
 
   [Category("Appearance")] public Color ModelColor { get; set; }
-	[Category("Appearance")] public Model Model { get; set; }
-  [Category("Appearance")] public Material ModelMaterial { get; set; }
+	[Category("Appearance")] public required Model Model { get; set; }
+  [Category("Appearance")] public Material? ModelMaterial { get; set; }
 
   [Category("Logic")] public NourishmentTypes NourishmentType { get; set; }
   [Category("Logic")] public int Price { get; set; }
