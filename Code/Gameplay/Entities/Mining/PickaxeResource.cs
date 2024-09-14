93d4d815960748434a6aa5671280bcf33519ade5
Yağız Akgün <berkay123d@outlook.com>
2024-09-14 19:02:13 +0300

    Revert "Mining System"
    This reverts commit 32eb8d68ef7a577b75d85a5be78d88089118f7a1.

--- Code/Gameplay/Entities/Mining/PickaxeResource.cs
+++
@@ -1,6 +0,0 @@
-
-[GameResource("Darkrp/Mining/Pickaxe Item", "pcx", "A basic pickaxe definition", IconBgColor = "#58c0e0", Icon = "ui/equipment/pickaxe.png")]
-public class PickaxeResource : EquipmentResource
-{
-	[Category( "Pickaxe" )] public float PickaxeDamage { get; set; }
-}
