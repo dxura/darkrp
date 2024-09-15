bd5314e83d0133a16d061b9f9cf051b016904a2d
Yağız Akgün <berkay123d@outlook.com>
2024-09-15 12:14:11 +0300

    Update Rock1Entity to use CurrentRockResource for health and model

--- Code/Gameplay/Entities/Mining/Rock1Entity.cs
+++ Code/Gameplay/Entities/Mining/Rock1Entity.cs
@@ -3,27 +3,43 @@ using Sandbox;
 using GameSystems.Jobs;
 namespace Dxura.Darkrp;
 
-[Title( "Rock" )]
-[Category( "Entities" )]
+[Title("Rock")]
+[Category("Entities")]
 public sealed class Rock1Entity : BaseEntity
 {
+    [Property] public RockResource CurrentRockResource { get; set; } = null!;
     protected override void OnStart()
     {
+        if (HealthComponent == null)
+        {
+            Log.Error("HealthComponent is null.");
+            return;
+        }
+
+        if (CurrentRockResource == null)
+        {
+            Log.Error("RockResource is null.");
+            return;
+        }
+
+        HealthComponent.Health = CurrentRockResource.Health;
+        GameObject.Components.GetInChildren<ModelRenderer>().Model = CurrentRockResource.Model;
+
 
     }
 
     protected override void OnUpdate()
     {
-        if(HealthComponent != null && HealthComponent.Health <= 0)
+        if (HealthComponent != null && HealthComponent.Health <= 0)
         {
             DestroyRock();
         }
-        
+
     }
 
     private void DestroyRock()
     {
-        Sound.Play( "kill_sound" );
+        Sound.Play("kill_sound");
         GameObject.Destroy();
     }
 }
--- Code/Gameplay/Entities/Mining/RockResource.cs
+++ Code/Gameplay/Entities/Mining/RockResource.cs
@@ -1,11 +1,10 @@
 
-[GameResource("Darkrp/Mining/Rock Item", "pcx", "A basic pickaxe definition", IconBgColor = "#58c0e0", Icon = "ui/equipment/pickaxe.png")]
-public class RockResource : EquipmentResource
+[GameResource("Darkrp/Mining/Rock Item", "rck", "A basic rock definition", IconBgColor = "#58c0e0", Icon = "ui/equipment/pickaxe.png")]
+public class RockResource : GameResource
 {
+	[Category( "General" )] public string? Name { get; set; }
+	[Category( "General" )] public string? Description { get; set; }
 	[Category( "General" )] public float Health { get; set; }
-	[Category( "General" )] public string Name { get; set; }
-	[Category( "General" )] public string Description { get; set; }
-	
-
-
+	[Category( "General" )] public Model? Model { get; set; }
+	[Category( "General" )] public SoundEvent? HitSound { get; set; }
 }
